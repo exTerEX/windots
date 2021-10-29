529914383ac0f66bcba31b13947065f0e168ce4a
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-10-29 12:48:49 +0200

    Added powershell profile

---
+++ powershell/profile.ps1
@@ -0,0 +1,6 @@
+# TODO
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "C:\Users\exterex\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
