90f590615347d7b42f9c6e32f0d6244bd43f3740
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-10-31 02:31:54 +0200

    Switched from ~ to HOME

--- setup.ps1
+++ setup.ps1
@@ -16,117 +16,117 @@ if ($exepolicy -ne "Unrestricted") {
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
 # Create directories
-If (-not (test-path "~/Documents/WindowsPowerShell")) { mkdir "~/Documents/WindowsPowerShell" }
+If (-not (test-path "$HOME/Documents/WindowsPowerShell")) { mkdir "$HOME/Documents/WindowsPowerShell" }
 
-If (-not (test-path "~/.aws")) { mkdir "~/.aws" }
-if (!(((Get-Item -Path "~/.aws" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
-    (Get-Item -Path "~/.aws" -Force).Attributes += "Hidden"
+If (-not (test-path "$HOME/.aws")) { mkdir "$HOME/.aws" }
+if (!(((Get-Item -Path "$HOME/.aws" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+    (Get-Item -Path "$HOME/.aws" -Force).Attributes += "Hidden"
 }
 
-If (-not (test-path "~/.azure")) { mkdir "~/.azure" }
-if (!(((Get-Item -Path "~/.azure" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
-    (Get-Item -Path "~/.azure" -Force).Attributes += "Hidden"
+If (-not (test-path "$HOME/.azure")) { mkdir "$HOME/.azure" }
+if (!(((Get-Item -Path "$HOME/.azure" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+    (Get-Item -Path "$HOME/.azure" -Force).Attributes += "Hidden"
 }
 
-If (-not (test-path "~/.config")) { mkdir "~/.config" }
-if (!(((Get-Item -Path "~/.config" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
-    (Get-Item -Path "~/.config" -Force).Attributes += "Hidden"
+If (-not (test-path "$HOME/.config")) { mkdir "$HOME/.config" }
+if (!(((Get-Item -Path "$HOME/.config" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+    (Get-Item -Path "$HOME/.config" -Force).Attributes += "Hidden"
 }
 
 # Powershell profile
-if (Test-Path -Path "~/Documents/WindowsPowerShell/profile.ps1") {
-    if (!(Get-Item "~/Documents/WindowsPowerShell/profile.ps1" -Force).LinkType -eq "SymbolicLink") {
+if (Test-Path -Path "$HOME/Documents/WindowsPowerShell/profile.ps1") {
+    if (!(Get-Item "$HOME/Documents/WindowsPowerShell/profile.ps1" -Force).LinkType -eq "SymbolicLink") {
         Write-Host "Changing profile.ps1 with symbolic link to $PWD/powershell/profile.ps1..." -ForegroundColor Blue
-        New-Item -ItemType SymbolicLink -Path "~/Documents/WindowsPowerShell/profile.ps1" -Target "$PWD/powershell/profile.ps1" -Force
+        New-Item -ItemType SymbolicLink -Path "$HOME/Documents/WindowsPowerShell/profile.ps1" -Target "$PWD/powershell/profile.ps1" -Force
     }
 } else {
     Write-Host "Symolic link to $PWD/powershell/profile.ps1..." -ForegroundColor Blue
-    New-Item -ItemType SymbolicLink -Path "~/Documents/WindowsPowerShell/profile.ps1" -Target "$PWD/powershell/profile.ps1" -Force
+    New-Item -ItemType SymbolicLink -Path "$HOME/Documents/WindowsPowerShell/profile.ps1" -Target "$PWD/powershell/profile.ps1" -Force
 }
 
 # Anaconda / Miniconda
-if (Test-Path -Path "~/.condarc") {
-    if (!(Get-Item "~/.condarc" -Force).LinkType -eq "SymbolicLink") {
+if (Test-Path -Path "$HOME/.condarc") {
+    if (!(Get-Item "$HOME/.condarc" -Force).LinkType -eq "SymbolicLink") {
         Write-Host "Changing file to a symbolic link to $PWD/.condarc..." -ForegroundColor Blue
-        New-Item -ItemType SymbolicLink -Path "~/.condarc" -Target "$PWD/miniconda/.condarc" -Force
+        New-Item -ItemType SymbolicLink -Path "$HOME/.condarc" -Target "$PWD/miniconda/.condarc" -Force
     }
 } else {
     Write-Host "Symolic link to $PWD/.condarc..." -ForegroundColor Blue
-    New-Item -ItemType SymbolicLink -Path "~/.condarc" -Target "$PWD/miniconda/.condarc" -Force
+    New-Item -ItemType SymbolicLink -Path "$HOME/.condarc" -Target "$PWD/miniconda/.condarc" -Force
 }
 
-if (!(((Get-Item -Path "~/.condarc" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
-    (Get-Item -Path "~/.condarc" -Force).Attributes += "Hidden"
+if (!(((Get-Item -Path "$HOME/.condarc" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+    (Get-Item -Path "$HOME/.condarc" -Force).Attributes += "Hidden"
 }
 
 # WSL
-if (Test-Path -Path "~/.wslconfig") {
-    if (!(Get-Item "~/.wslconfig" -Force).LinkType -eq "SymbolicLink") {
+if (Test-Path -Path "$HOME/.wslconfig") {
+    if (!(Get-Item "$HOME/.wslconfig" -Force).LinkType -eq "SymbolicLink") {
         Write-Host "Changing file to a symbolic link to $PWD/.wslconfig..." -ForegroundColor Blue
-        New-Item -ItemType SymbolicLink -Path "~/.wslconfig" -Target "$PWD/wsl/windows/.wslconfig" -Force
+        New-Item -ItemType SymbolicLink -Path "$HOME/.wslconfig" -Target "$PWD/wsl/windows/.wslconfig" -Force
     }
 } else {
     Write-Host "Symolic link to $PWD/.wslconfig..." -ForegroundColor Blue
-    New-Item -ItemType SymbolicLink -Path "~/.wslconfig" -Target "$PWD/wsl/windows/.wslconfig" -Force
+    New-Item -ItemType SymbolicLink -Path "$HOME/.wslconfig" -Target "$PWD/wsl/windows/.wslconfig" -Force
 }
 
-if (!(((Get-Item -Path "~/.wslconfig" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
-    (Get-Item -Path "~/.wslconfig" -Force).Attributes += "Hidden"
+if (!(((Get-Item -Path "$HOME/.wslconfig" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+    (Get-Item -Path "$HOME/.wslconfig" -Force).Attributes += "Hidden"
 }
 
 # Git
-if (Test-Path -Path "~/.gitconfig") {
-    if (!(Get-Item "~/.gitconfig" -Force).LinkType -eq "SymbolicLink") {
+if (Test-Path -Path "$HOME/.gitconfig") {
+    if (!(Get-Item "$HOME/.gitconfig" -Force).LinkType -eq "SymbolicLink") {
         Write-Host "Changing file to a symbolic link to $PWD/.gitconfig..." -ForegroundColor Blue
-        New-Item -ItemType SymbolicLink -Path "~/.gitconfig" -Target "$PWD/git/.gitconfig" -Force
+        New-Item -ItemType SymbolicLink -Path "$HOME/.gitconfig" -Target "$PWD/git/.gitconfig" -Force
     }
 } else {
     Write-Host "Symolic link to $PWD/.gitconfig..." -ForegroundColor Blue
-    New-Item -ItemType SymbolicLink -Path "~/.gitconfig" -Target "$PWD/git/.gitconfig" -Force
+    New-Item -ItemType SymbolicLink -Path "$HOME/.gitconfig" -Target "$PWD/git/.gitconfig" -Force
 }
 
-if (!(((Get-Item -Path "~/.gitconfig" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
-    (Get-Item -Path "~/.gitconfig" -Force).Attributes += "Hidden"
+if (!(((Get-Item -Path "$HOME/.gitconfig" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+    (Get-Item -Path "$HOME/.gitconfig" -Force).Attributes += "Hidden"
 }
 
 # Bash
-if (Test-Path -Path "~/.bashrc") {
-    if (!(Get-Item "~/.bashrc" -Force).LinkType -eq "SymbolicLink") {
+if (Test-Path -Path "$HOME/.bashrc") {
+    if (!(Get-Item "$HOME/.bashrc" -Force).LinkType -eq "SymbolicLink") {
         Write-Host "Changing file to a symbolic link to $PWD/.bashrc..." -ForegroundColor Blue
-        New-Item -ItemType SymbolicLink -Path "~/.bashrc" -Target "$PWD/bash/.bashrc" -Force
+        New-Item -ItemType SymbolicLink -Path "$HOME/.bashrc" -Target "$PWD/bash/.bashrc" -Force
     }
 } else {
     Write-Host "Symolic link to $PWD/.bashrc..." -ForegroundColor Blue
-    New-Item -ItemType SymbolicLink -Path "~/.bashrc" -Target "$PWD/bash/.bashrc" -Force
+    New-Item -ItemType SymbolicLink -Path "$HOME/.bashrc" -Target "$PWD/bash/.bashrc" -Force
 }
 
-if (!(((Get-Item -Path "~/.bashrc" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
-    (Get-Item -Path "~/.bashrc" -Force).Attributes += "Hidden"
+if (!(((Get-Item -Path "$HOME/.bashrc" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+    (Get-Item -Path "$HOME/.bashrc" -Force).Attributes += "Hidden"
 }
 
-if (Test-Path -Path "~/.bash_profile") {
-    if (!(Get-Item "~/.bash_profile" -Force).LinkType -eq "SymbolicLink") {
+if (Test-Path -Path "$HOME/.bash_profile") {
+    if (!(Get-Item "$HOME/.bash_profile" -Force).LinkType -eq "SymbolicLink") {
         Write-Host "Changing file to a symbolic link to $PWD/.bash_profile..." -ForegroundColor Blue
-        New-Item -ItemType SymbolicLink -Path "~/.bash_profile" -Target "$PWD/bash/.bash_profile" -Force
+        New-Item -ItemType SymbolicLink -Path "$HOME/.bash_profile" -Target "$PWD/bash/.bash_profile" -Force
     }
 } else {
     Write-Host "Symolic link to $PWD/.bash_profile..." -ForegroundColor Blue
-    New-Item -ItemType SymbolicLink -Path "~/.bash_profile" -Target "$PWD/bash/.bash_profile" -Force
+    New-Item -ItemType SymbolicLink -Path "$HOME/.bash_profile" -Target "$PWD/bash/.bash_profile" -Force
 }
 
-if (!(((Get-Item -Path "~/.bash_profile" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
-    (Get-Item -Path "~/.bash_profile" -Force).Attributes += "Hidden"
+if (!(((Get-Item -Path "$HOME/.bash_profile" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+    (Get-Item -Path "$HOME/.bash_profile" -Force).Attributes += "Hidden"
 }
 
 # Scoop
-if (Test-Path -Path "~/.config/scoop/config.json") {
-    if (!(Get-Item "~/.config/scoop/config.json" -Force).LinkType -eq "SymbolicLink") {
-        Write-Host "Changing file to a symbolic link to $PWD/~/.config/scoop/config.json..." -ForegroundColor Blue
-        New-Item -ItemType SymbolicLink -Path "~/.config/scoop/config.json" -Target "$PWD/scoop/config.json" -Force
+if (Test-Path -Path "$HOME/.config/scoop/config.json") {
+    if (!(Get-Item "$HOME/.config/scoop/config.json" -Force).LinkType -eq "SymbolicLink") {
+        Write-Host "Changing file to a symbolic link to $PWD/$HOME/.config/scoop/config.json..." -ForegroundColor Blue
+        New-Item -ItemType SymbolicLink -Path "$HOME/.config/scoop/config.json" -Target "$PWD/scoop/config.json" -Force
     }
 } else {
     Write-Host "Symolic link to $PWD/scoop/config.json..." -ForegroundColor Blue
-    New-Item -ItemType SymbolicLink -Path "~/.config/scoop/config.json" -Target "$PWD/scoop/config.json" -Force
+    New-Item -ItemType SymbolicLink -Path "$HOME/.config/scoop/config.json" -Target "$PWD/scoop/config.json" -Force
 }
 
 #Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
@@ -134,13 +134,13 @@ if (Test-Path -Path "~/.config/scoop/config.json") {
 
 #scoop install git-crypt
 
-if (!(((Get-Item -Path "~/scoop" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
-    (Get-Item -Path "~/scoop" -Force).Attributes += "Hidden"
+if (!(((Get-Item -Path "$HOME/scoop" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+    (Get-Item -Path "$HOME/scoop" -Force).Attributes += "Hidden"
 }
 
 # Winget
-if (Test-Path -Path "~/") {
-    if (!(Get-Item "~/" -Force).LinkType -eq "SymbolicLink") {
+if (Test-Path -Path "$HOME/") {
+    if (!(Get-Item "$HOME/" -Force).LinkType -eq "SymbolicLink") {
         Write-Host "Changing file to a symbolic link to $PWD/winget/settings.json..." -ForegroundColor Blue
         New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json" -Target "$PWD/winget/settings.json" -Force
     }
@@ -150,29 +150,29 @@ if (Test-Path -Path "~/") {
 }
 
 # pymol
-If (-not (test-path "~/.pymol")) { mkdir "~/.pymol" }
-if (!(((Get-Item -Path "~/.pymol" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
-    (Get-Item -Path "~/.pymol" -Force).Attributes += "Hidden"
+If (-not (test-path "$HOME/.pymol")) { mkdir "$HOME/.pymol" }
+if (!(((Get-Item -Path "$HOME/.pymol" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+    (Get-Item -Path "$HOME/.pymol" -Force).Attributes += "Hidden"
 }
 
-If (-not (test-path "~/.pymol/cache")) { mkdir "~/.pymol/cache" }
-If (-not (test-path "~/.pymol/scripts")) { mkdir "~/.pymol/scripts" }
+If (-not (test-path "$HOME/.pymol/cache")) { mkdir "$HOME/.pymol/cache" }
+If (-not (test-path "$HOME/.pymol/scripts")) { mkdir "$HOME/.pymol/scripts" }
 
-if (Test-Path -Path "~/pymolrc.pml") {
-    if (!(Get-Item "~/pymolrc.pml" -Force).LinkType -eq "SymbolicLink") {
+if (Test-Path -Path "$HOME/pymolrc.pml") {
+    if (!(Get-Item "$HOME/pymolrc.pml" -Force).LinkType -eq "SymbolicLink") {
         Write-Host "Changing file to a symbolic link to $PWD/pymol/pymolrc.pml..." -ForegroundColor Blue
-        New-Item -ItemType SymbolicLink -Path "~/pymolrc.pml" -Target "$PWD/pymol/pymolrc.pml" -Force
+        New-Item -ItemType SymbolicLink -Path "$HOME/pymolrc.pml" -Target "$PWD/pymol/pymolrc.pml" -Force
     }
 } else {
     Write-Host "Symolic link to $PWD/pymol/pymolrc.pml..." -ForegroundColor Blue
-    New-Item -ItemType SymbolicLink -Path "~/pymolrc.pml" -Target "$PWD/pymol/pymolrc.pml" -Force
+    New-Item -ItemType SymbolicLink -Path "$HOME/pymolrc.pml" -Target "$PWD/pymol/pymolrc.pml" -Force
 }
 
-if (!(((Get-Item -Path "~/pymolrc.pml" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
-    (Get-Item -Path "~/pymolrc.pml" -Force).Attributes += "Hidden"
+if (!(((Get-Item -Path "$HOME/pymolrc.pml" -Force).Attributes.ToString() -Split ", ") -Contains "Hidden")) {
+    (Get-Item -Path "$HOME/pymolrc.pml" -Force).Attributes += "Hidden"
 }
 
-cd "~/.pymol/scripts"
+cd "$HOME/.pymol/scripts"
 git clone "https://github.com/exTerEX/pymol-ramachandran.git"
 cd $launch_pwd
 
