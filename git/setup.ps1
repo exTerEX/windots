26c6c9ecc5c1affa56399d5c4cb5203d67d4e2ea
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-11-27 15:44:52 +0100

    Fixed errors

--- git/setup.ps1
+++ git/setup.ps1
@@ -7,10 +7,10 @@ New-Directory -Path "$HOME\.git" -Hide
 Set-Softlink -Path "$HOME\.gitconfig" -Target "$PSScriptRoot\.gitconfig" -Hide
 
 # Create softlink to '.gitignore' and hide it.
-Set-Softlink -Path "$HOME\.git\.gitignore" -Target "$PSScriptRoot\.gitignore" -Hide
+Set-Softlink -Path "$HOME\.git\.gitignore" -Target "$PSScriptRoot\.gitignore"
 
 # Create softlink to '.gitattributes' and hide it.
-Set-Softlink -Path "$HOME\.git\.gitattributes" -Target "$PSScriptRoot\.gitattributes" -Hide
+Set-Softlink -Path "$HOME\.git\.gitattributes" -Target "$PSScriptRoot\.gitattributes"
 
 # Create softlink to '.gitmessage' and hide it.
-Set-Softlink -Path "$HOME\.giot\.gitmessage" -Target "$PSScriptRoot\.gitmessage" -Hide
+Set-Softlink -Path "$HOME\.git\.gitmessage" -Target "$PSScriptRoot\.gitmessage"
