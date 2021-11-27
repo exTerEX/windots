c9493e9ecd3b83103d70c6778bf09fc8ab782e06
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-11-27 15:28:34 +0100

    Added more .git* files and changed setup file

--- git/setup.ps1
+++ git/setup.ps1
@@ -1,4 +1,16 @@
 #!/usr/bin/env pwsh
 
-# Create softlink to '.gitignore' and hide it.
+# Create related directories
+New-Directory -Path "$HOME\.git" -Hide
+
+# Create softlink to '.gitconfig' and hide it.
 Set-Softlink -Path "$HOME\.gitconfig" -Target "$PSScriptRoot\.gitconfig" -Hide
+
+# Create softlink to '.gitignore' and hide it.
+Set-Softlink -Path "$HOME\.git\.gitignore" -Target "$PSScriptRoot\.gitignore" -Hide
+
+# Create softlink to '.gitattributes' and hide it.
+Set-Softlink -Path "$HOME\.git\.gitattributes" -Target "$PSScriptRoot\.gitattributes" -Hide
+
+# Create softlink to '.gitmessage' and hide it.
+Set-Softlink -Path "$HOME\.giot\.gitmessage" -Target "$PSScriptRoot\.gitmessage" -Hide
