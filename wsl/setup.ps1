40dd6518daba0c543c84d2775170e5e64b4c35e8
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-11-27 02:57:09 +0100

    Removed Linux-side configuration files; Moved into wsldots repository

--- wsl/setup.ps1
+++ wsl/setup.ps1
@@ -2,3 +2,18 @@
 
 # Create softlink to '.wslconfig' and hide it.
 Set-Softlink -Path "$HOME\.wslconfig" -Target "$PSScriptRoot\windows\.wslconfig" -Hide
+
+# Setup system for WSL2
+wsl --install
+
+# Set default version
+wsl --set-default-version 2
+
+# Install distributions
+wsl --install --distribution Ubuntu
+
+# Set default distribution
+wsl --set-default Ubuntu
+
+# Output summary
+wsl --status
