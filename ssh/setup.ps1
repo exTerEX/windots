0a3175d66fb281891e1389db2b4d73ac08e3d0a7
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-11-28 02:39:47 +0100

    Stopped tracking known_hosts

--- ssh/setup.ps1
+++ ssh/setup.ps1
@@ -9,8 +9,5 @@ Set-Softlink -Path "$HOME\.ssh\id_rsa" -Target "$PSScriptRoot\id_rsa"
 # Create softlink to 'id_rsa.pub'.
 Set-Softlink -Path "$HOME\.ssh\id_rsa.pub" -Target "$PSScriptRoot\id_rsa.pub"
 
-# Create softlink to 'knwon_hosts'.
-Set-Softlink -Path "$HOME\.ssh\known_hosts" -Target "$PSScriptRoot\known_hosts"
-
 # Create softlink to 'config'.
 Set-Softlink -Path "$HOME\.ssh\config" -Target "$PSScriptRoot\config"
