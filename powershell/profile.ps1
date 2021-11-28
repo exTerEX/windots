7d3509cc31236673562c943643cacea834e50dc6
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-11-28 19:02:26 +0100

    Added module installations

---
+++ powershell/profile.ps1
@@ -0,0 +1,8 @@
+# Import posh-git
+Import-Module posh-git
+
+# Import oh-my-posh
+Import-Module oh-my-posh
+
+# Set oh-my-posh theme
+Set-PoshPrompt -Theme craver
--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -1,9 +1,19 @@
 #!/usr/bin/env pwsh
 
 # Create new machine scoped variables.
-
 Set-Item -Path env:powershell -Value "C:\Windows\System32\WindowsPowerShell\v1.0"
 Set-Item -Path env:pwsh -Value env:powershell
+
+# Install PackageManagement
+Install-Module -Name PackageManagement -Repository PSGallery
+
+# Install posh-git
+Install-Module posh-git -Repository PSGallery
+
+# Install oh-my-posh
+Install-Module oh-my-posh -Repository PSGallery
+
+# Install fonts
 
 # Create softlink to 'profile.ps1'.
 Set-Softlink -Path "$env:powershell\profile.ps1" -Target "$PSScriptRoot\profile.ps1"
