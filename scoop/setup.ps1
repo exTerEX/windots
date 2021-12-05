79ecab048a8b2c6cf24da95e8edc21308b062c56
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-05 22:05:24 +0100

    Added automatic installation of scoop dependencies

--- scoop/setup.ps1
+++ scoop/setup.ps1
@@ -6,3 +6,13 @@ New-Directory -Path "$HOME\scoop" -Hide
 
 # Create softlink to 'config.json' and hide it.
 Set-Softlink -Path "$HOME\.config\scoop\config.json" -Target "$PSScriptRoot\config.json"
+
+# Install scoop
+
+# Install scoop dependencies
+scoop install git-crypt
+scoop install 7zip
+scoop install curl
+
+# Hide related files
+Hide-File "$HOME\.lesshst"
