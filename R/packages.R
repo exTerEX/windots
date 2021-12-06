db92a937a95f799324a63b7c2224198dfb910130
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-06 20:37:04 +0100

    Added installation of R and useful packages.

---
+++ R/packages.R
@@ -0,0 +1,14 @@
+r <- getOption("repos")
+r["CRAN"] <- "https://cloud.r-project.org/"
+options(repos = r)
+
+install.packages("rmarkdown")
+install.packages("languageserver")
+install.packages("httpgd")
+install.packages("tidyverse")
+install.packages("ggpubr")
+install.packages("ggsci")
+install.packages("gggenes")
+install.packages("ggtext")
+install.packages("ggpmisc")
+install.packages("ggcorrplot")
--- R/setup.ps1
+++ R/setup.ps1
@@ -1,4 +1,24 @@
 #!/usr/bin/env pwsh
 
+# Install R
+winget install RProject.R --architecture x64 --accept-package-agreements --accept-source-agreements
+
 # Create related directories
 New-Directory -Path "$HOME\.vscode-R" -Hide
+
+# Install R packages
+# Find folder to Rscript
+$PATH = "$env:programfiles\R"
+$NAME = Get-ChildItem -Path $PATH -Name
+
+# TODO: If more then 1 select largest number
+
+$RPATH = "$PATH\$NAME\bin"
+
+# Set R-path to environment path
+# TODO: Check if RPath already is added?
+Set-Item -Path $env:path -Value ($env:path + ";$RPATH")
+
+Set-Location $RPATH
+Rscript.exe R/packages.R
+Set-Location $PSScriptRoot
