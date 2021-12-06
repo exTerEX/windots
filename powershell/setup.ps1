395a6eba45d93f5e67352e7c5ea5e27a0a5db0e6
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-06 22:22:08 +0100

    Added automatic installation of necessary fonts.

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -14,6 +14,11 @@ Install-Module posh-git -Repository PSGallery
 Install-Module oh-my-posh -Repository PSGallery
 
 # Install fonts
+$Uri = "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip"
+$FilePath = Get-Zip -Uri $Uri -Extract
+
+$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+Get-ChildItem -Path $FilePath -Recurse -include *.ttf | ForEach-Object { $fonts.CopyHere($_.fullname) }
 
 # Create softlink to 'profile.ps1'.
 Set-Softlink -Path "$env:powershell\profile.ps1" -Target "$PSScriptRoot\profile.ps1"
