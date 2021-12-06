896ee0ebccb324e9d8128f96207d8daa5a713cfd
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-06 20:52:02 +0100

    Changed syntax.

--- R/packages.R
+++ R/packages.R
@@ -2,13 +2,19 @@ r <- getOption("repos")
 r["CRAN"] <- "https://cloud.r-project.org/"
 options(repos = r)
 
-install.packages("rmarkdown")
-install.packages("languageserver")
-install.packages("httpgd")
-install.packages("tidyverse")
-install.packages("ggpubr")
-install.packages("ggsci")
-install.packages("gggenes")
-install.packages("ggtext")
-install.packages("ggpmisc")
-install.packages("ggcorrplot")
+pkgs <- c(
+    "rmarkdown",
+    "languageserver",
+    "httpgd",
+    "tidyverse",
+    "ggpubr",
+    "ggsci",
+    "gggenes",
+    "ggtext",
+    "ggpmisc",
+    "ggcorrplot"
+)
+
+pkgs <- unique(pkgs)
+
+install.packages(pkgs)
