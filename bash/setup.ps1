587c822beeda43784458ca822ff8919483b9d942
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-07 01:00:07 +0100

    Facilitated new files

--- bash/setup.ps1
+++ bash/setup.ps1
@@ -1,7 +1,19 @@
 #!/usr/bin/env pwsh
 
-# Create softlink to '.bashrc' and hide it.
-Set-Softlink -Path "$HOME\.bashrc" -Target "$PSScriptRoot\.bashrc" -Hide
+# Create directories
+New-Directory -Path "$HOME\.config\bash"
 
 # Create softlink to '.bash_profile' and hide it.
 Set-Softlink -Path "$HOME\.bash_profile" -Target "$PSScriptRoot\.bash_profile" -Hide
+
+# Create softlink to '.bashrc' and hide it.
+Set-Softlink -Path "$HOME\.bashrc" -Target "$PSScriptRoot\.bashrc" -Hide
+
+# Create softlink to '.bash_aliases' and hide it.
+Set-Softlink -Path "$HOME\.config\bash\.bash_aliases" -Target "$PSScriptRoot\.bash_aliases"
+
+# Create softlink to '.bash_exports' and hide it.
+Set-Softlink -Path "$HOME\.config\bash\.bash_exports" -Target "$PSScriptRoot\.bash_exports"
+
+# Create softlink to '.bash_functions' and hide it.
+Set-Softlink -Path "$HOME\.config\bash\.bash_functions" -Target "$PSScriptRoot\.bash_functions"
