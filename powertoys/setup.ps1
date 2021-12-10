9598dc608ea01a3fd13817b4005b6869db8f525c
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-10 21:46:42 +0100

    Added installer and test to setup script of powertoys

--- powertoys/setup.ps1
+++ powertoys/setup.ps1
@@ -1,5 +1,11 @@
 #!/usr/bin/env pwsh
-#!/usr/bin/env pwsh
+
+# Install powertoys
+$KEY = "{DAA80B28-95F8-4E2C-BED4-3446474AEDA4}"
+$REGPATH = "HKLM:Software\Microsoft\Windows\CurrentVersion\Uninstall"
+if (!(((Get-ChildItem $REGPATH) | Where-Object { $_."Name" -like "*$KEY*" } ).Length -gt 0)) {
+    winget install XP89DCGQ3K6VLD --architecture x64 --accept-package-agreements
+}
 
 # Install
 winget install XP89DCGQ3K6VLD --accept-package-agreements --accept-source-agreements
