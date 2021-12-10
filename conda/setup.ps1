e6d5733237cdce14ea908b8a31e870abd949c520
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-10 13:27:20 +0100

    Added conda installation in script.

--- conda/setup.ps1
+++ conda/setup.ps1
@@ -1,4 +1,9 @@
 #!/usr/bin/env pwsh
 
+# Install Miniconda
+if (!(where.exe conda)) {
+    winget install Anaconda.Miniconda3 --scope=user --architecture=x64
+}
+
 # Create softlink to '.condarc' and hide it.
 Set-Softlink -Path "$HOME\.condarc" -Target "$PSScriptRoot\.condarc" -Hide
