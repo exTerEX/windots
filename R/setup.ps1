73d36c6d4c9a66857699b9b4002770ea0acd6058
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-11 23:51:58 +0100

    Minor changes to setup.

--- R/setup.ps1
+++ R/setup.ps1
@@ -14,15 +14,19 @@ if (!(((Get-ChildItem $REGPATH) | Where-Object { $_."Name" -like "*R*" } ).Lengt
 $RPATH = "$env:programfiles\R\R-*\bin\x64" | Convert-Path
 $REGREXPATH = [regex]::Escape($RPATH)
 $ARRAYPATH = [Environment]::GetEnvironmentVariable("Path", "Machine") -split ";" |
-    Where-Object { $_ -notMatch "^$REGREXPATH\\?" }
+  Where-Object { $_ -notMatch "^$REGREXPATH\\?" }
 [Environment]::SetEnvironmentVariable("Path", ($ARRAYPATH + $RPATH) -join ";", "Machine")
 
 # Create softlink to '.Rprofile'.
 $RPROFILEPATH = "$HOME\.config\R\.Rprofile"
 Set-Softlink -Path $RPROFILEPATH -Target "$PSScriptRoot\.Rprofile"
 
+# Set library path
+$RLIBSPATH = "$HOME\.config\R\lib\4"
+
 # Set environment variables
 [System.Environment]::SetEnvironmentVariable("R_PROFILE_USER", $RPROFILEPATH, "User")
+[System.Environment]::SetEnvironmentVariable("R_LIBS_USER", $RLIBSPATH, "User")
 
 # Install R packages
-Rscript R/packages.R
+# FIXME: Rscript R/packages.R
