8800b36656b17868ffe69aa15c9cf56eba47f698
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-11 01:38:28 +0100

    Seperated system and global configuration.

--- git/setup.ps1
+++ git/setup.ps1
@@ -1,23 +1,21 @@
 #!/usr/bin/env pwsh
 
-# Create related directories
-New-Directory -Path "$HOME\.config\git"
-
-$BASEPATH = "$HOME\.config\git"
-
-# Create softlink to '.gitconfig' and hide it.
-Set-Softlink -Path "$HOME\.gitconfig" -Target "$PSScriptRoot\.gitconfig" -Hide
-
-# Create softlink to '.gitignore' and hide it.
-Set-Softlink -Path "$BASEPATH\.gitignore" -Target "$PSScriptRoot\.gitignore"
-
-# Create softlink to '.gitattributes' and hide it.
-Set-Softlink -Path "$BASEPATH\.gitattributes" -Target "$PSScriptRoot\.gitattributes"
-
-# Create softlink to '.gitmessage' and hide it.
-Set-Softlink -Path "$BASEPATH\.gitmessage" -Target "$PSScriptRoot\.gitmessage"
-
 # Install git
 if (!(where.exe git)) {
     winget install Git.Git --scope=machine --architecture=x64
 }
+
+# Create related directories
+New-Directory -Path "$HOME\.config\git"
+
+# Create softlink to 'config' and hide it.
+Set-Softlink -Path "$HOME\.config\git\config" -Target "$PSScriptRoot\config"
+
+# Create softlink to '.gitignore' and hide it.
+Set-Softlink -Path "$HOME\.config\git\.gitignore" -Target "$PSScriptRoot\.gitignore"
+
+# Create softlink to '.gitattributes' and hide it.
+Set-Softlink -Path "$HOME\.config\git\.gitattributes" -Target "$PSScriptRoot\.gitattributes"
+
+# Create softlink to '.gitmessage' and hide it.
+Set-Softlink -Path "$HOME\.config\git\.gitmessage" -Target "$PSScriptRoot\.gitmessage"
