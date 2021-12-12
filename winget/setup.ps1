68672a94a20ef63fd92bc2de8abb8ff635800f64
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-12-12 01:01:07 +0100

    Added descriptive comment.

--- winget/setup.ps1
+++ winget/setup.ps1
@@ -1,4 +1,7 @@
 #!/usr/bin/env pwsh
 
-# Create softlink to 'settings.json'.
-Set-Softlink -Path "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json" -Target "$PSScriptRoot\settings.json"
+# Create softlink to the Winget configuration file to keep specific settings
+# and activated experimental features in sync.
+$Path = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe"
+$Target = "$PSScriptRoot\settings.json"
+Set-Softlink -Path "$Path\LocalState\settings.json" -Target $Target
