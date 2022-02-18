74a66e8d1b52300d043ef0b50e7b9b02420fe599
exTerEX <30706568+exTerEX@users.noreply.github.com>
2022-02-18 22:51:58 +0100

    Added check to prevent installation of VSCode extensions unless available

--- vscode/setup.ps1
+++ vscode/setup.ps1
@@ -11,24 +11,26 @@ if (!(where.exe code)) {
 Set-Softlink -Path "$env:appdata\Code\User\settings.json" -Target "$PSScriptRoot\settings.json"
 
 # Install packages
-code --install-extension docsmsft.docs-markdown
-code --install-extension tamasfe.even-better-toml
-code --install-extension grapecity.gc-excelviewer
-code --install-extension miguelsolorio.fluent-icons
-code --install-extension github.codespaces
-code --install-extension github.remotehub
-code --install-extension github.github-vscode-theme
-code --install-extension github.vscode-pull-request-github
-code --install-extension ms-vscode.vscode-typescript-next
-code --install-extension ms-toolsai.jupyter
-code --install-extension james-yu.latex-workshop
-code --install-extension ms-vscode.powershell
-code --install-extension ms-vscode-remote.remote-containers
-code --install-extension esbenp.prettier-vscode
-code --install-extension ms-python.python
-code --install-extension ms-python.vscode-pylance
-code --install-extension ikuyadeu.r
-code --install-extension mechatroner.rainbow-csv
-code --install-extension gruntfuggly.todo-tree
-code --install-extension editorconfig.editorconfig
-code --install-extension ms-azuretools.vscode-docker
+if (where.exe code) {
+    code --install-extension docsmsft.docs-markdown
+    code --install-extension tamasfe.even-better-toml
+    code --install-extension grapecity.gc-excelviewer
+    code --install-extension miguelsolorio.fluent-icons
+    code --install-extension github.codespaces
+    code --install-extension github.remotehub
+    code --install-extension github.github-vscode-theme
+    code --install-extension github.vscode-pull-request-github
+    code --install-extension ms-vscode.vscode-typescript-next
+    code --install-extension ms-toolsai.jupyter
+    code --install-extension james-yu.latex-workshop
+    code --install-extension ms-vscode.powershell
+    code --install-extension ms-vscode-remote.remote-containers
+    code --install-extension esbenp.prettier-vscode
+    code --install-extension ms-python.python
+    code --install-extension ms-python.vscode-pylance
+    code --install-extension ikuyadeu.r
+    code --install-extension mechatroner.rainbow-csv
+    code --install-extension gruntfuggly.todo-tree
+    code --install-extension editorconfig.editorconfig
+    code --install-extension ms-azuretools.vscode-docker
+}
