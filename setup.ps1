0a201dc1b36f31e2f4bf5871626a532a1f746a9e
exTerEX <30706568+exTerEX@users.noreply.github.com>
2022-04-18 12:39:32 +0200

    Added function

--- setup.ps1
+++ setup.ps1
@@ -78,6 +78,17 @@ function Get-Zip {
     }
 }
 
+function Find-Installed( $programName ) {
+    $x86_check = ((Get-ChildItem "HKLM:Software\Microsoft\Windows\CurrentVersion\Uninstall") |
+        Where-Object { $_."Name" -like "*$programName*" } ).Length -gt 0;
+
+    if (Test-Path 'HKLM:Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall') {
+        $x64_check = ((Get-ChildItem "HKLM:Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+            Where-Object { $_."Name" -like "*$programName*" } ).Length -gt 0;
+    }
+    return $x86_check -or $x64_check;
+}
+
 # Trust PSrepository
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
